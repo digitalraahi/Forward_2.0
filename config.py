645b320bdad8c97deb727174c6085d4f9c415c91
digitalraahi <135437639+digitalraahi@users.noreply.github.com>
2023-10-17 08:05:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ class Config:
     BOT_SESSION = os.environ.get("BOT_SESSION", "Forward_BOT") 
     OWNER_ID = os.environ.get("OWNER_ID", "")
     DATABASE_URI = os.environ.get("DATABASE_URI", "")
-    DATABASE_NAME = os.environ.get("DATABASE_NAME","Cluster0")
+    DATABASE_NAME = os.environ.get("DATABASE_NAME","hemant")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Forward_data')
-    SESSION = os.environ.get("SESSION", "Forward_Session")
+    SESSION = os.environ.get("SESSION", "BQGMcpgAbaN39PQVmksmwDtmwFYNtRmk4waFNtRA4illWqR3lZlOg4JJvXIttSPYAQBVHJwYhdSErtSFYEspsGUw1jGVqng-g_CxGE4IDHKugySCF7znBGmjTmptEwfi2ebWEpyJtRbXJMo1bi3tXLisNGwqXoIiYY_j1EVnd4LPWmyUCCKas1ncs3zo1VgU22CLRMpquFbVc43amuCTsUgBZPOpV7FGWnxRJQ2xKzz5xBBP7EbSCvY4Uh8lyWe0eGZuHLDrVSzGsx9F_VTxZTsNFQ1NmOwlreYB8_-zdJIgNIDQ-ssAMozJvH0b4990akI1g5HmdujDRFkE_D1F-MKXi0zBcQAAAAE1FccZAA")
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", ""))
-    BOT_USERNAME= os.environ.get("BOT_USERNAME", None)
+    BOT_USERNAME= os.environ.get("BOT_USERNAME", "fdboxbot")
 
 
 def LOGGER(name: str) -> logging.Logger:
